f6c563acf311c3a06ee061559833692d09e3e8ae
薛纪昌 <jichangee@gmail.com>
2019-06-04 11:28:09 +0800

    token发行表单验证

--- src/views/Token/publish.vue
+++ src/views/Token/publish.vue
@@ -4,22 +4,22 @@
       <div slot="header" class="clearfix">
         <span>发行</span>
       </div>
-      <el-form label-width="80px">
-        <el-form-item label="名称">
+      <el-form label-width="80px" :rules="rules" :model="form" ref="formModal">
+        <el-form-item label="名称" prop="name">
           <el-input v-model="form.name"></el-input>
         </el-form-item>
-        <el-form-item label="简介">
-          <el-input type="textarea" v-model="form.symbol"></el-input>
+        <el-form-item label="Symbol" prop="symbol">
+          <el-input type="text" v-model="form.symbol"></el-input>
         </el-form-item>
-        <el-form-item label="小数位数">
+        <el-form-item label="小数位数" prop="decimals">
           <el-input-number v-model="form.decimals" controls-position="right" :min="0" :max="20"></el-input-number>
         </el-form-item>
-        <el-form-item label="总量">
+        <el-form-item label="总量" prop="totalSupply">
           <el-input type="number" v-model="form.totalSupply"></el-input>
         </el-form-item>
-        <div class="handle-container" style="text-align: right;">
+        <el-form-item class="handle-container" style="text-align: right;">
           <el-button type="primary" @click="submit">提交</el-button>
-        </div>
+        </el-form-item>
       </el-form>
     </el-card>
     <el-dialog
@@ -53,6 +53,21 @@ export default class TokenPublish extends Vue{
     decimals: 2,
     totalSupply: ''
   };
+  rules: any = {
+    name: [
+      { required: true, message: '请输入名称', trigger: 'blur' }
+    ],
+    symbol: [
+      { required: true, message: '请输入Symbol', trigger: 'blur' },
+      { min: 4, max: 8, message: '长度在 4 到 8 个字符', trigger: 'blur' }
+    ],
+    decimals: [
+      { required: true, message: '请输入小数位数', trigger: 'blur' },
+    ],
+    totalSupply: [
+      { required: true, message: '请输入总量', trigger: 'blur' },
+    ],
+  }
   dialogVisible: boolean = false;
   successMsg: any = {
     address: '',
@@ -60,25 +75,31 @@ export default class TokenPublish extends Vue{
   }
   
   submit() {
-    const { form } = this;
-    const loading = this.$loading({
-      lock: true,
-      text: 'ArtShare发行中',
-      spinner: 'el-icon-loading',
-      background: 'rgba(0, 0, 0, 0.7)'
+    this.$refs.formModal.validate((valid: boolean) => {
+      console.log(valid);
+      if (!valid) {
+        return;
+      }
+      const { form } = this;
+      const loading = this.$loading({
+        lock: true,
+        text: 'ArtShare发行中',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0, 0, 0, 0.7)'
+      });
+      http.token.publish({
+        ...form
+      }).then((res: any) => {
+        const info = res.info;
+        loading.close();
+        this.$notify.success('提交成功');
+        this.successMsg.address = info.contractAddress;
+        this.successMsg.totalSupply = info.totalSupply;
+        this.dialogVisible = true;
+      }).catch(() => {
+        loading.close();
+      })
     });
-    http.token.publish({
-      ...form
-    }).then((res: any) => {
-      const info = res.info;
-      loading.close();
-      this.$notify.success('提交成功');
-      this.successMsg.address = info.contractAddress;
-      this.successMsg.totalSupply = info.totalSupply;
-      this.dialogVisible = true;
-    }).catch(() => {
-      loading.close();
-    })
   }
 
   finish() {
